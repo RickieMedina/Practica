b3c385a0dbf6071f90887742305d6601f4ccb770
Ricky_Medina <ricardo.medina@kdsoft.io>
2021-12-20 01:37:11 -0300

    Se agrega ejercicio ok

---
+++ Ejercicio Canguro/Ejercicio Canguro/Canguro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Canguro
+{
+    class Canguro
+    {
+        public int Velocidad { get; set; }
+        public int Ubicacion { get; set; }
+
+        public int Saltar(int velocidad)
+        {
+            int posicion_actual = this.Ubicacion += velocidad;
+            return posicion_actual;
+        }
+
+    }
+}
